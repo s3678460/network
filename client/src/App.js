28eee72c9e81a2cbd974b0378edbe6c89663a644
s3678460 <40556454+s3678460@users.noreply.github.com>
2020-03-15 14:43:44 +1100

    minh

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import RouterURL from "./components/RouterURL/RouterURL";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar"
